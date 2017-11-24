9ef091d30fd01e83260b99eaeb7b95f326d8faaa
BIG_CS <thurakit.big@gmail.com>
2017-11-24 17:35:50 +0700

    Edit beginner

--- server/index.js
+++ server/index.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+var datas = require('./datas');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 var datas = require('./datas');
-
-app.get('/', (req, res) => {
-    res.json(datas)
-})
 
 
 
