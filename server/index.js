1682c3b74572567dc1b588db30df612449d6adde
Supawadee <58160595@go.buu.ac.th>
2017-11-24 18:19:50 +0700

    finish post-all

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const getAll = require('./get-all');
 var datas = require('./datas');
-
+var post= require('./post');
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 //----------------code Here -------------
 
+app.use('/', post);
 //---------------------------------------
 module.exports = app;
